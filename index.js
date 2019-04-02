dcb7140f0d057791e0370d203bac58813eaf60cc
Jahaziel <jahaziel.vaz@gmail.com>
2019-04-01 19:17:55 -0600

    All 4 tests are completed

--- index.js
+++ index.js
@@ -59,31 +59,33 @@ let passedFilter = inventors.filter(criteria => (criteria.year > 1700 || criteri
 //Map Test: Dynamically calculate how many years each inventor lived, and dynamically have a message for each of them that tells you how many years they lived;
 
 
-let valueAssigner = () => {
-    let years = inventors.map(yearsLived => yearsLived.passed - yearsLived.year);
-    return [...years]
-}
-
-let valuesArray = valueAssigner();
-
-  let yearsCalculator = () => {
-    for(var i; i < valuesArray.length; i++){
-    inventors.yearsLived = inventors.map(values => values.yearsLived = valuesArray[i])
-  }
-  return inventors.yearsLived;
-}
-
-yearsCalculator()
+// let valueAssigner = () => {
+//     let years = inventors.map(yearsLived => yearsLived.passed - yearsLived.year);
+//     return [...years]
+// }
+//
+// let valuesArray = valueAssigner();
+//
+//   let yearsCalculator = () => {
+//     for(var i; i < valuesArray.length; i++){
+//     inventors.yearsLived = inventors.map(values => values.yearsLived = valuesArray[i])
+//   }
+//   return inventors.yearsLived;
+// }
+//
+// yearsCalculator()
 
 
 
 // yearsCalculator()
 //NOT YET: The result was. The map feature passed every value to each of the values in the array. I believe I need to create a separate function that takes in the mapped result and spreads the values to each value in the array.
+//Need to study further, how to break down and parse mapped info to go into a newly assigned property of the desired
 
 //Sort Test: Sort inventors by who lived first
-let livedFirst = inventors.sort((a, b) => a.years > b.year ? 1 : -1);
+let livedFirst = inventors.sort((a, b) => a.year > b.year ? 1 : -1);
 
 // console.table(livedFirst);
+//PASSED TEST: In the beginning it didn't work, but that's simply because I had a typo, and it wasn't an understanding mistake, or anything of the sort. Great job me.
 
 
 //Reduce Test: Put all the people into one single string
@@ -91,4 +93,6 @@ let allPeople = people.reduce((result, subject) => {
   result + subject;
 }, '');
 
-// console.log(allPeople);
+console.log(allPeople);
+//NOT YET: I feel like I could have passed this test if I was working with an array of objects, but i don't fully know how to work with a simple string array.
+//Need to find out if I even could use the reduce method on string arrays.
